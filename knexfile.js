cfc5d10c8d928b8cc586899f4741fc911886d6cf
mzstevens69 <stevensmike36@yahoo.com>
2021-02-07 18:41:31 -0600

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -24,15 +24,14 @@ module.exports = {
     production: {
         client: 'pg',
         connection: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        },
+        
         pool: {
-            min: 0,
+            min: 2,
             max: 10
         },
         migrations: {
-            directory: './database/migrations'
+            directory: './database/migrations',
+            tableName: "knex_migrations"
         }
         
     },
@@ -40,12 +39,12 @@ module.exports = {
         client: "pg",
         connection: process.env.DATABASE_TEST,
         pool: {
-          min: 0,
+          min: 2,
           max: 10
         },
         migrations: {
           directory: "./database/migrations",
-          // tableName: "knex_migrations"
+          tableName: "knex_migrations"
         },
         seeds: {
           directory: "./database/seeds"

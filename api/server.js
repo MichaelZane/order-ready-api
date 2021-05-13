b31f9e1ac9382d314a684cf4ffbf2454cfddf461
MichaelZane <stevensmike36@yahoo.com>
2021-05-13 00:36:11 -0500

    cors issue

--- api/server.js
+++ api/server.js
@@ -10,15 +10,18 @@ server.use(express.urlencoded({extended: false}))
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
-
-const corsOptions = {
-    origin: "*"
-}
-
+// Routes
 server.use('/api/order', orderRouter)
 server.use('/api', messageRouter)
 
-server.get('/', cors(corsOptions),(req, res) => {
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+})
+
+server.get('/',(req, res) => {
     res.status(200).json({
         api: "If your seeing this, I am a working api"
     })

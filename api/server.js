b18f2ce9ebc43ef68e01081e13be0efb0f384f81
MichaelZane <stevensmike36@yahoo.com>
2021-05-13 00:24:19 -0500

    bodyparser fix

--- api/server.js
+++ api/server.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
-const bodyParser = require('body-parser')
 const orderRouter = require('../order-router')
 const messageRouter = require('../sendMessage')
 
 const server = express()
 
-server.use(bodyParser.urlencoded({extended: false}))
+server.use(express.urlencoded({extended: false}))
 server.use(express.json())
 server.use(helmet())
 server.use(cors())

5247701ac82470e789ae6535390ec31aeb499617
MichaelZane <stevensmike36@yahoo.com>
2022-04-21 19:23:23 -0500

    cors

--- api/server.js
+++ api/server.js
@@ -14,12 +14,12 @@ server.use(cors())
 server.use('/api/order', orderRouter)
 server.use('/api', messageRouter)
 
-// server.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-//       next();
-// })
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+})
 
 server.get('/',(req, res) => {
     res.status(200).json({

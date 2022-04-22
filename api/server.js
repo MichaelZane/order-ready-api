f8448b17e1e1369b1be4c26a477aaa967ed89a4a
MichaelZane <stevensmike36@yahoo.com>
2022-04-21 19:31:51 -0500

    cors

--- api/server.js
+++ api/server.js
@@ -9,17 +9,17 @@ const server = express()
 server.use(express.urlencoded({extended: false}))
 server.use(express.json())
 server.use(helmet())
-server.use(cors())
+
+const corsOptions ={
+    origin:'https://order-ready.netlify.app', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+server.use(cors(corsOptions));
+
 // Routes
 server.use('/api/order', orderRouter)
 server.use('/api', messageRouter)
-
-server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-})
 
 server.get('/',(req, res) => {
     res.status(200).json({
